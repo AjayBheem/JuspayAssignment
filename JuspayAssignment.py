ddea7a9d7a8639386ad0f6b617da469b1637589d
AjayBheem <119755654+AjayBheem@users.noreply.github.com>
2025-06-07 14:13:30 +0530

    Update JuspayAssignment.py

--- JuspayAssignment.py
+++ JuspayAssignment.py
@@ -1,101 +1,123 @@
 import re
 from collections import defaultdict, deque
+import operator
 
-class ReactiveSimulator:
+class ReactiveVariable:
+    """Represents a reactive variable with expression and dependencies."""
+    def __init__(self, name, expression):
+        self.name = name
+        self.expression = expression
+        self.dependencies = set()
+        self.value = None
+
+    def extract_dependencies(self):
+        # Match all variable names (e.g., a, b)
+        tokens = re.findall(r'\b[a-zA-Z_]\w*\b', self.expression)
+        self.dependencies = set(filter(lambda x: not x.isdigit() and x != 'input', tokens))
+
+class ReactiveSystem:
     def __init__(self):
-        self.expressions = {}      # var -> expression
-        self.values = {}           # var -> current value
-        self.dependencies = {}     # var -> set of dependent vars
-        self.reverse_deps = defaultdict(set)  # var -> who depends on var
-        self.topo_order = []       # variables in topological order
+        self.variables = {}
+        self.reverse_dependencies = defaultdict(set)
+        self.topo_order = []
 
-    def parse_var(self, line):
-        # e.g., var b = a + 2
-        _, var_name, _, expr = line.split(' ', 3)
-        self.expressions[var_name] = expr
-        tokens = re.findall(r'\b[a-zA-Z_]\w*\b', expr)
-        deps = set(filter(lambda x: x != 'input', tokens))
-        self.dependencies[var_name] = deps
-        for dep in deps:
-            self.reverse_deps[dep].add(var_name)
+    def add_variable(self, line):
+        _, name, _, expr = line.split(' ', 3)
+        var = ReactiveVariable(name, expr)
+        var.extract_dependencies()
+        self.variables[name] = var
+        for dep in var.dependencies:
+            self.reverse_dependencies[dep].add(name)
 
-    def topological_sort(self):
-        in_degree = defaultdict(int)
-        for var in self.expressions:
-            for dep in self.dependencies.get(var, []):
-                in_degree[var] += 1
+    def build_topo_order(self):
+        in_degree = {var: 0 for var in self.variables}
+        for var in self.variables.values():
+            for dep in var.dependencies:
+                in_degree[var.name] += 1
 
-        queue = deque([var for var in self.expressions if in_degree[var] == 0])
+        queue = deque([v for v in self.variables if in_degree[v] == 0])
         self.topo_order = []
 
         while queue:
-            var = queue.popleft()
-            self.topo_order.append(var)
-            for dep in self.reverse_deps.get(var, []):
-                in_degree[dep] -= 1
-                if in_degree[dep] == 0:
-                    queue.append(dep)
+            current = queue.popleft()
+            self.topo_order.append(current)
+            for dependent in self.reverse_dependencies.get(current, []):
+                in_degree[dependent] -= 1
+                if in_degree[dependent] == 0:
+                    queue.append(dependent)
 
-    def evaluate_expr(self, expr):
-        try:
-            return eval(expr, {}, self.values)
-        except:
-            return None
+    def evaluate_expression(self, expr, values):
+        # Custom safe parser and evaluator (supports +, -, *, //)
+        tokens = expr.split()
+        stack = []
+
+        ops = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '//': operator.floordiv,
+        }
+
+        # Convert infix to postfix (Shunting Yard algorithm can be used for complex cases)
+        postfix = []
+        for token in tokens:
+            if token in ops:
+                postfix.append(token)
+            else:
+                postfix.append(values.get(token, token))
+
+        # Evaluate postfix expression
+        result_stack = []
+        for token in postfix:
+            if str(token).isdigit():
+                result_stack.append(int(token))
+            elif token in values:
+                result_stack.append(values[token])
+            elif token in ops:
+                b = result_stack.pop()
+                a = result_stack.pop()
+                result_stack.append(ops[token](a, b))
+        return result_stack[0] if result_stack else None
 
     def evaluate_all(self):
-        for var in self.topo_order:
-            if self.expressions[var] == 'input':
-                continue
-            expr = self.expressions[var]
-            self.values[var] = self.evaluate_expr(expr)
+        values = {}
+        for var_name in self.topo_order:
+            var = self.variables[var_name]
+            if var.expression == 'input':
+                values[var_name] = var.value  # Set manually via event
+            else:
+                values[var_name] = self.evaluate_expression(var.expression, values)
+            var.value = values[var_name]
 
-    def process_event(self, var, value):
-        self.values[var] = int(value)
+    def handle_event(self, var_name, new_value):
+        self.variables[var_name].value = int(new_value)
 
-        # BFS to find all affected variables
-        queue = deque([var])
         affected = set()
-
+        queue = deque([var_name])
         while queue:
             current = queue.popleft()
-            for dep in self.reverse_deps.get(current, []):
+            for dep in self.reverse_dependencies.get(current, []):
                 if dep not in affected:
                     affected.add(dep)
                     queue.append(dep)
 
-        affected = sorted(affected, key=lambda x: self.topo_order.index(x))
-        for var in affected:
-            expr = self.expressions[var]
-            self.values[var] = self.evaluate_expr(expr)
+        self.evaluate_all()
 
-        # Print all updated values
-        print(f"{var} = {value}")
-        for v in affected:
-            print(f"{v} = {self.values[v]}")
+        # Print all affected variables in topo order
+        print(f"{var_name} = {new_value}")
+        for var in self.topo_order:
+            if var == var_name or var in affected:
+                print(f"{var} = {self.variables[var].value}")
 
     def process(self, lines):
         for line in lines:
-            line = line.strip()
             if line.startswith('var'):
-                self.parse_var(line)
+                self.add_variable(line)
 
-        self.topological_sort()
+        self.build_topo_order()
         self.evaluate_all()
 
         for line in lines:
-            line = line.strip()
             if line.startswith('event'):
                 _, var, _, val = line.split()
-                self.process_event(var, val)
-
-# Example usage:
-input_lines = [
-    "var a = input",
-    "var b = a + 2",
-    "var c = b * 3",
-    "event a = 4",
-    "event a = 6"
-]
-
-sim = ReactiveSimulator()
-sim.process(input_lines)
+                self.handle_event(var, val)
